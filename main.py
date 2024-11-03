c58425a0a8f6b1ffbe8b2a575d5fa2fdeb2885a9
ImBharathwaj <bharathwajist@gmail.com>
2024-11-03 12:57:58 +0530

    Updated

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-import re
-import requests
-import urllib
-import urllib3
+from robot import *
+from link_extractor import *
+from urllib.request import urlopen
+from image_extractor import *
 
+
+if __name__ == '__main__':
+    # prepare('http://www.apress.com/robot.txt')
+    # print(is_allowed('http://hajba.hu/category/software-development/java-software-development/','bookbot'))
+    target_url = 'http://www.apress.com/'
+    apress = download_page(target_url)
+    # links = extract_links(apress)
+    # for link in links:
+    #     print(link)
+    image_locations = extract_image_locations(apress)
+
+    for src in image_locations:
+        print(src)
